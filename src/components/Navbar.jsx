041f51bcdbe6dab7dc91e0cb06621bc35167f250
Ravi Gupta <142169363+guptaravimp@users.noreply.github.com>
2025-07-06 02:03:09 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import Link from 'next/link'
 function Navbar() {
   const isdarkMode = useSelector((state) => state.theme.darkMode)
   return (
-    <div className={` ${isdarkMode ? ' bg-gradient-to-br from-gray-900 via-gray-800 to-black text-white' : 'bg-gradient-to-br from-blue-50 to-indigo-100 text-gray-900'} w-full mx-auto flex justify-between items-center p-1 `}>
+    <div className={` ${isdarkMode ? ' bg-gradient-to-br from-gray-900 to-gray-800 via-gray-900 text-white' : 'bg-gradient-to-br from-blue-50 to-indigo-100 text-gray-900'} w-full mx-auto flex justify-between items-center p-1 `}>
       <div className="w-full flex justify-between items-center p-4">
         <div className='flex gap-2'>
           <Link href={"/"}> <div className="w-8 h-8 bg-gradient-to-r from-blue-600 to-purple-600 rounded-lg flex items-center justify-center">
