3166ca850a2c6d818e4d31a6d799de7f931944a9
Ravi Gupta <142169363+guptaravimp@users.noreply.github.com>
2025-07-06 02:08:09 +0530

    Update LeadForm.jsx

--- src/components/LeadForm.jsx
+++ src/components/LeadForm.jsx
@@ -60,7 +60,7 @@ export default function LeadForm() {
         <div className='w-full '>
             <form onSubmit={handleSubmit(onSubmit)} className='  flex flex-col justify-evenly items-center  gap-1'>
                 <div className=' flex flex-col justify-between items-center gap-3'>
-                    <h3 className='text-2xl font-semibold'>Get Your Free Strategy Call</h3>
+                      <h3 className="text-4xl font-bold  bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">Get Your Free Strategy Call</h3>
                     <p className='text-xl text-gray-400 '>Fill out the form below and we'll get back to you within 24 hours</p>
 
 
